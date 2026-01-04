68d7192eb04d22f435684ecf738b774a4350d536
Ramil Khasanshin <khasanshin.ramil@list.ru>
2026-01-05 00:52:35 +0300

    refactor: open listener listen functions

--- main.c
+++ main.c
@@ -6,53 +6,74 @@
 #include <string.h>
 
 #include <stdio.h>
+#include <stdbool.h>
 
-int main(int argc, char** argv) {
+#define RW_BACKLOG 10
 
+typedef struct RW_Connection {
+	int fd;
+	uint16_t port;
+} RW_Connection;
+
+bool rw_open_listener(RW_Connection* wc, uint16_t port) {
 	int server_fd = socket(AF_INET, SOCK_STREAM, 0);
 	if (server_fd == -1) {
 		perror("Socket error.");
-		return 1;
+		return false;
 	}
+
+	wc->fd = server_fd;
+	wc->port = port;
 
 	struct sockaddr_in server_addr;
 	server_addr.sin_family = AF_INET;
 	server_addr.sin_addr.s_addr = INADDR_ANY;
-	server_addr.sin_port = htons(6969);
+	server_addr.sin_port = htons(port);
 
 	if (bind(server_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
 		perror("Binding error.");
-		return 1;
+		return false;
 	}
 
-	if (listen(server_fd, 10) < 0) {
+	if (listen(server_fd, RW_BACKLOG) < 0) {
 		perror("Listen error.");
-		return 1;
+		return false;
 	}
 
-	printf("hello: %d\n", 6969);
+	return true;
+}
+
+void rw_listen(RW_Connection* wc) {
+	struct sockaddr_in client_addr;
+	socklen_t client_len = sizeof(client_addr);
+	int client_fd = accept(wc->fd, (struct sockaddr*)&client_addr, &client_len);
+	if (client_fd < 0) {
+		perror("Accept error.");
+		return;
+	}
+	printf("connected:%s\n", inet_ntoa(client_addr.sin_addr));
+	char buf[1024] = {0};
+	read(client_fd, buf, sizeof(buf) - 1);
+	printf("req:%s\n", buf);
+	char resp[] =
+		"HTTP/1.0 200 OK\r\n"
+		"Content-Type: text/html\r\n"
+		"Content-Length: 5\r\n"
+		"\r\n"
+		"hello";
+	write(client_fd, resp, strlen(resp));
+}
+
+int main(int argc, char** argv) {
+
+	RW_Connection wc = {0};
+
+	rw_open_listener(&wc, 6969);
 
 	while (1) {
-		struct sockaddr_in client_addr;
-		socklen_t client_len = sizeof(client_addr);
-		int client_fd = accept(server_fd, (struct sockaddr*)&client_addr, &client_len);
-		if (client_fd < 0) {
-			perror("Accept error.");
-			return 1;
-		}
-		printf("connected:%s\n", inet_ntoa(client_addr.sin_addr));
-		char buf[1024] = {0};
-		read(client_fd, buf, sizeof(buf) - 1);
-		printf("req:%s\n", buf);
-		char resp[] =
-			"HTTP/1.0 200 OK\r\n"
-			"Content-Type: text/html\r\n"
-			"Content-Length: 5\r\n"
-			"\r\n"
-			"hello";
-		write(client_fd, resp, strlen(resp));
+		rw_listen(&wc);
 	}
 
-	close(server_fd);
+	close(wc.fd);
 	return 0;
 }
